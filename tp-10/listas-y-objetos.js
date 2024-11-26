9dc995bc54bc2b5ff678c9831ccfc39fbde908df
santi <163933056+mvvp4@users.noreply.github.com>
2024-11-26 00:42:25 -0300

    act 10 y 11

--- tp-10/listas-y-objetos.js
+++ tp-10/listas-y-objetos.js
@@ -24,16 +24,6 @@ var listaPersonasEjemplo = [
         "documento": 45678
     },
 ];
-
-/**
- * 01 - ordenarPorApellido
- * 
- * Recibe
- * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`.
- * 
- * Retorna: 
- * - el mismo listado, ordenado alfabéticamente por el apellido de la persona 
- */
 function ordenarPorApellido(listaDePersonas) {
 
     return listaDePersonas.sort((persona1, persona2) => {
@@ -51,59 +41,25 @@ function ordenarPorApellido(listaDePersonas) {
 
 }
 console.log("ordenarPorApellido()", ordenarPorApellido(listaPersonasEjemplo));
-
-/**
- * 02 - soloNombres
- * 
- * Recibe
- * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
- * 
- * Retorna: 
- * - una lista de strings, con sólo los nombres de las personas
- */
 function soloNombres(listaDePersonas) {
-    // 
+    return listaDePersonas.map(persona => persona.nombre);
 }
 console.log("soloNombres()", soloNombres(listaPersonasEjemplo));
 
-/**
- * 03 - promedioEdades
- * 
- * Recibe
- * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
- * 
- * Retorna: 
- * - un numero, con el cálculo del promedio de las edades
- */
 function promedioEdades(listaDePersonas) {
-    //
+    let totalEdad = listaDePersonas.reduce((acumulador, persona) => acumulador + persona.edad, 0);
+    return totalEdad / listaDePersonas.length;
 }
 console.log("promedioEdades()", promedioEdades(listaPersonasEjemplo));
 
-/**
- * 04 - soloMayoresDeEdad
- * 
- * Recibe
- * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
- * 
- * Retorna: 
- * - una lista, array, conteniendo solamente las personas con más de 18 años
- */
 function soloMayoresDeEdad(listaDePersonas) {
-    //
+    return listaDePersonas.filter(persona => persona.edad > 18);
 }
 console.log("soloMayoresDeEdad()", soloMayoresDeEdad(listaPersonasEjemplo));
 
-/**
- * 05 - laPersonaMayor
- * 
- * Recibe
- * - `listaDePersonas`: una lista, array, con objetos de la forma `persona`
- * 
- * Retorna: 
- * - una objeto con la persona de mayor edad en todo el listado. En caso de que hayan 2 personas con la misma edad, se puede retornar la primera que aparezca en el listado.
- */
 function laPersonaMayor(listaDePersonas) {
-    //
+    return listaDePersonas.reduce((mayor, persona) => 
+        persona.edad > mayor.edad ? persona : mayor
+    );
 }
 console.log("laPersonaMayor()", laPersonaMayor(listaPersonasEjemplo));
